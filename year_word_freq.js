9a605fee37d893aac1fa82828c3e7058bfe3cf65
Leah Xu <leahmuzixu@gmail.com>
2014-12-05 06:55:46 -0500

    Imported Lyric Dictionary

--- year_word_freq.js
+++ year_word_freq.js
@@ -11,3 +11,15 @@ var reduce = function(key, values) {
 }
 
 db.songs.mapReduce( map, reduce, { out: "songs_temp" } );
+
+
+
+Input: (word_stem, (year, count))
+Output: mean
+
+var reduce = function(word, values) {
+	year, count = values
+	count_avg = Array.avg(count)
+
+	return {"average": count_avg}
+}
